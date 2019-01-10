86662453055ac4ddf2151bb5a27b5fee33a3648a
Kelsey Tobin <etobinkelsey@gmail.com>
2019-01-10 09:49:13 -0700

    fixed file path to js issue, submit button working correctly, pushing user input to topics array

--- assets/app.js
+++ assets/app.js
@@ -1,52 +1,79 @@
  //create topics array
  var topics = ["Biggie Smalls", "Snoop Dog", "Phife Dawg", "Busta Rhymes", "Drake"];
 
+ 
  //function loops through topics array, creates button, and appends to page
- function callCreate() {
+ function renderButtons() {
+     $("#button-display").empty();
+     
      for (var i = 0; i < topics.length; i++) {
          var btn = $("<button>");
-         btn.addClass("name")
+         btn.addClass("rapper-btn");
+         btn.addClass("btn btn-primary");
          btn.attr("data-name", topics[i]);
          btn.text(topics[i]);
          $("#button-display").append(btn);
      }
  };
 
- //execute
- $(document).ready(function () {
-     callCreate();
-
+ function displayGifs() {
      //event listener for buttons appended to html
      $("button").on("click", function () {
-         console.log(this);
 
-         //create variable to include inside url query- this refers to button that was clicked
-         var rapper = $(this).attr("data-name");
+        // //empty gifs from previous button click
+        $("#gif-display").empty();
 
-         //construct url to search Giphy for gifs related to topics
-         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-             rapper + "&api_key=HRHgIG4zqFVU91tc8vZifNXXdYGAobgR&limit=10";
+        //create variable to include inside url query- this refers to button that was clicked
+        var person = $(this).attr("data-name");
 
-         //perform ajax GET request
-         $.ajax({
-                 url: queryURL,
-                 method: "GET"
-             })
+        //construct url to search Giphy for gifs related to topics
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            person + "&api_key=HRHgIG4zqFVU91tc8vZifNXXdYGAobgR&limit=10";
 
-             //after data comes back from the API
-             .then(function (response) {
-                 var results = response.data;
+        //perform ajax GET request
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
 
-                 for (var i = 0; i < results.length; i++) {
-                     var gifDiv = $("<div>");
-                     var rating = results[i].rating;
-                     var p = $("<p>").text("Rating: " + rating);
-                     var personImage = $("<img>");
-                     personImage.attr("src", results[i].images.fixed_height.url);
-                     gifDiv.append(p);
-                     gifDiv.append(personImage);
-                     $("#gif-display").prepend(gifDiv)
-                 }
-             })
-     })
+            //after data comes back from the API
+            .then(function (response) {
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var personImage = $("<img>");
+                    personImage.attr("src", results[i].images.fixed_height.url);
+                    gifDiv.append(p);
+                    gifDiv.append(personImage);
+                    $("#gif-display").prepend(gifDiv)
+                }
+            });
+    });
+ };
+
+
+ //execute
+ $(document).ready(function () {
+    
+    $("#add-rapper").on("click", function(event){
+        event.preventDefault();
+        
+        var rapper = $("#rapper-input").val().trim();
+        
+        topics.push(rapper);
+
+        $("#rapper-input").val("");
+        
+        renderButtons();
+
+        console.log(topics);
+    });
+    
+    $(document).on("click", ".rapper-btn", displayGifs);
+
+    renderButtons();
+   
  });
